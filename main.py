b4d9bf21252cc83769648663e1314edf446eccdd
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-04 18:25:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,18 +15,6 @@ bot = Client(
 CHAT_ID = os.environ.get('CHAT_ID')
 owner = int(os.environ.get('OWNER'))
 
-def bytes(size: float) -> str:
-    """humanize size"""
-    if not size:
-        return ""
-    power = 1024
-    t_n = 0
-    power_dict = {0: " ", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
-    while size > power:
-        size /= power
-        t_n += 1
-    return "{:.2f} {}B".format(size, power_dict[t_n])
-
 def call_back_in_filter(data):
     return filters.create(
         lambda flt, _, query: flt.data in query.data,
@@ -42,41 +30,6 @@ def start(_,message):
 @bot.on_message(filters.command('help'))
 def help(_,message):
     message.reply_text('💯 If you want, you can contact us using this format \n\n Ex:- /request Hello, I need a help')
-
-@bot.on_message(filters.command("speedtest") & ~filters.edited)
-async def statsguwid(_, message):
-    m = await message.reply_text("Running Speed test..........")
-    try:
-        test = speedtest.Speedtest()
-        test.get_best_server()
-        m = await m.edit("Running Download SpeedTest")
-        test.download()
-        m = await m.edit("Running Upload SpeedTest")
-        test.upload()
-        test.results.share()
-        result = test.results.dict()
-    except Exception as e:
-        return await m.edit(e)
-    m = await m.edit("Sharing SpeedTest Results")
-    path = wget.download(result["share"])
-
-    output = f"""**Speedtest Results**
-    
-<u>**Client:**</u>
-**__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
-  
-<u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
-**__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
-**__Ping:__** {result['ping']}"""
-    msg = await bot.send_photo(
-        chat_id=message.chat.id, photo=path, caption=output
-    )
-    os.remove(path)
-    await m.delete()
     
 @bot.on_message(filters.command('request'))
 def req(_,message):
