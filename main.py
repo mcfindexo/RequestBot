a3d864da12fef1dce2d8a5d2a8523900d9398a35
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-04 18:39:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from pyrogram import filters , Client
 from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
 import os
+import logging
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
 bot = Client(
     "notesbot",
     api_id=os.environ['API_ID'],
